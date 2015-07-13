1c1e072d8d281ee4806800270e98a114efed7d57
adarapata <efb.youdie@gmail.com>
2015-07-14 02:11:10 +0900

    テストも書いてみる

---
+++ Editor/Tests/Base/LifeTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+using Game.Base;
+using UniRx;
+
+namespace Game.Tests.Base
+{
+    [TestFixture]
+    [Category("LifeTest")]
+    public class LifeTest
+    {
+        [Test]
+        public void LifeOverStreamTest()
+        {
+            Life life = new Life(10);
+            life.lifeOverStream.Subscribe(l => {
+                Assert.LessOrEqual(l, 0);
+            });
+            life.Damage(15);
+        }
+    }
+}
