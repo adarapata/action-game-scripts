a644d56db4d4b5e1a2279659aaa5c5717f6bc48b
adarapata <efb.youdie@gmail.com>
2015-07-14 02:10:42 +0900

    体力を管理するクラス作成

---
+++ Base/Life.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+namespace Game.Base {
+    public class Life
+    {
+        public IObservable<int> lifeOverStream { get; private set; }
+        public ReactiveProperty<int> currentPoint { get; private set; }
+
+        public Life (int initialPoint)
+        {
+            currentPoint = new ReactiveProperty<int>(initialPoint);
+            lifeOverStream = currentPoint.Where(x => x <= 0).Take(1).AsObservable();
+        }
+
+        public void Damage(int point)
+        {
+            currentPoint.Value -= point;
+        }
+    }
+}
